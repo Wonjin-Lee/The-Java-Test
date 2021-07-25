468afc77a668c877e16d91fd0923aaf774598825
Wonjin-Lee <claude17@naver.com>
2021-07-25 17:40:47 +0900

    Upload Project

---
+++ src/test/java/com/wonjin/thejavatest/TheJavaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wonjin.thejavatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
